4971d72676772835022270eb868916575cc99498
siva-aicoder <p59611423@gmail.com>
2025-11-06 10:40:05 +0000

    Auto: push local modified contents

--- simple-reactjs-app-master/src/CustomerDetails.js
+++ simple-reactjs-app-master/src/CustomerDetails.js
@@ -7,50 +7,62 @@ export default class CustomerDetails extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = { showMore: false, customerDetails: null };
   }
 
   //Function which is called when the component loads for the first time
   componentDidMount() {
-    this.getCustomerDetails(this.props.val)
+    this.getCustomerDetails(this.props.val);
   }
 
-  //Function which is called whenver the component is updated
+  //Function which is called whenever the component is updated
   componentDidUpdate(prevProps) {
-
     //get Customer Details only if props has changed
     if (this.props.val !== prevProps.val) {
-      this.getCustomerDetails(this.props.val)
+      this.getCustomerDetails(this.props.val);
     }
   }
 
-  //Function to Load the customerdetails data from json.
+  //Function to Load the customer details data from json.
   getCustomerDetails(id) {
     axios.get('assets/samplejson/customer' + id + '.json').then(response => {
-      this.setState({customerDetails: response})
-    })
+      this.setState({customerDetails: response});
+    });
+  };
+
+  toggleMoreInfo = () => {
+    this.setState(prevState => ({ showMore: !prevState.showMore }));
   };
 
   render() {
     if (!this.state.customerDetails)
-      return (<p>Loading Data</p>)
+      return (<p>Loading Data</p>);
+
+    const { data } = this.state.customerDetails;
     return (<div className="customerdetails">
       <Panel bsStyle="info" className="centeralign">
         <Panel.Heading>
-          <Panel.Title componentClass="h3">{this.state.customerDetails.data.name}</Panel.Title>
+          <Panel.Title componentClass="h3">{data.name}</Panel.Title>
         </Panel.Heading>
         <Panel.Body>
-          <p>Name : {this.state.customerDetails.data.name}</p>
-          <p>Email : {this.state.customerDetails.data.email}</p>
-          <p>Phone : {this.state.customerDetails.data.phone}</p>
-          <p>City : {this.state.customerDetails.data.city}</p>
-          <p>State : {this.state.customerDetails.data.state}</p>
-          <p>Country : {this.state.customerDetails.data.country}</p>
-          <p>Organization : {this.state.customerDetails.data.organization}</p>
-          <p>Job Profile : {this.state.customerDetails.data.jobProfile}</p>
-          <p>Additional Info : {this.state.customerDetails.data.additionalInfo}</p>
+          <p>Name : {data.name}</p>
+          <p>Email : {data.email}</p>
+          <p>Phone : {data.phone}</p>
+          <React.Fragment>
+            <button onClick={this.toggleMoreInfo}>{this.state.showMore ? 'See Less' : 'See More'}</button>
+            {this.state.showMore && (
+              <React.Fragment>
+                <p>City : {data.city}</p>
+                <p>State : {data.state}</p>
+                <p>Country : {data.country}</p>
+                <p>Organization : {data.organization}</p>
+                <p>Job Profile : {data.jobProfile}</p>
+                <p>Additional Info : {data.additionalInfo}</p>
+              </React.Fragment>
+            )}
+          </React.Fragment>
         </Panel.Body>
       </Panel>
-    </div>)
+    </div>);
   }
 }
