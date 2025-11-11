07de0eff2d300d2a7da8b79a8a5deb4d8874c857
siva-aicoder <p59611423@gmail.com>
2025-11-11 09:43:42 +0000

    Auto: push local modified contents

--- simple-reactjs-app-master/src/CustomerDetails.js
+++ simple-reactjs-app-master/src/CustomerDetails.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
-import Panel from 'react-bootstrap/lib/Panel'
-import axios from 'axios'
+import Panel from 'react-bootstrap/lib/Panel';
+import axios from 'axios';
 
 //This Component is a child Component of Customers Component
 export default class CustomerDetails extends Component {
@@ -15,7 +15,7 @@ export default class CustomerDetails extends Component {
     this.getCustomerDetails(this.props.val)
   }
 
-  //Function which is called whenver the component is updated
+  //Function which is called whenever the component is updated
   componentDidUpdate(prevProps) {
 
     //get Customer Details only if props has changed
@@ -24,7 +24,7 @@ export default class CustomerDetails extends Component {
     }
   }
 
-  //Function to Load the customerdetails data from json.
+  //Function to Load the customer details data from json.
   getCustomerDetails(id) {
     axios.get('assets/samplejson/customer' + id + '.json').then(response => {
       this.setState({customerDetails: response})
@@ -33,24 +33,27 @@ export default class CustomerDetails extends Component {
 
   render() {
     if (!this.state.customerDetails)
-      return (<p>Loading Data</p>)
-    return (<div className="customerdetails">
-      <Panel bsStyle="info" className="centeralign">
-        <Panel.Heading>
-          <Panel.Title componentClass="h3">{this.state.customerDetails.data.name}</Panel.Title>
-        </Panel.Heading>
-        <Panel.Body>
-          <p>Name : {this.state.customerDetails.data.name}</p>
-          <p>Email : {this.state.customerDetails.data.email}</p>
-          <p>Phone : {this.state.customerDetails.data.phone}</p>
-          <p>City : {this.state.customerDetails.data.city}</p>
-          <p>State : {this.state.customerDetails.data.state}</p>
-          <p>Country : {this.state.customerDetails.data.country}</p>
-          <p>Organization : {this.state.customerDetails.data.organization}</p>
-          <p>Job Profile : {this.state.customerDetails.data.jobProfile}</p>
-          <p>Additional Info : {this.state.customerDetails.data.additionalInfo}</p>
-        </Panel.Body>
-      </Panel>
-    </div>)
+      return (<p>Loading Data</p>);
+    return (<React.Fragment>
+      <div className="customerdetails">
+        <button style={{marginRight: '8px', fontSize: '16px'}} onClick={() => this.props.onLeftArrowClick()}>&lt;</button>
+        <Panel bsStyle="info" className="centeralign">
+          <Panel.Heading>
+            <Panel.Title componentClass="h3">{this.state.customerDetails.data.name}</Panel.Title>
+          </Panel.Heading>
+          <Panel.Body>
+            <p>Name : {this.state.customerDetails.data.name}</p>
+            <p>Email : {this.state.customerDetails.data.email}</p>
+            <p>Phone : {this.state.customerDetails.data.phone}</p>
+            <p>City : {this.state.customerDetails.data.city}</p>
+            <p>State : {this.state.customerDetails.data.state}</p>
+            <p>Country : {this.state.customerDetails.data.country}</p>
+            <p>Organization : {this.state.customerDetails.data.organization}</p>
+            <p>Job Profile : {this.state.customerDetails.data.jobProfile}</p>
+            <p>Additional Info : {this.state.customerDetails.data.additionalInfo}</p>
+          </Panel.Body>
+        </Panel>
+      </div>
+    </React.Fragment>);
   }
 }
