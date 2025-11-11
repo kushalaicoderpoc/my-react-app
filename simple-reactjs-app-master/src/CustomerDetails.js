700fa3b86788998456488aef09fa110734ab2483
siva-aicoder <p59611423@gmail.com>
2025-11-11 10:31:23 +0000

    Auto: push local modified contents

--- simple-reactjs-app-master/src/CustomerDetails.js
+++ simple-reactjs-app-master/src/CustomerDetails.js
@@ -1,56 +1,87 @@
 import React, {Component} from 'react';
-import Panel from 'react-bootstrap/lib/Panel'
-import axios from 'axios'
+import Panel from 'react-bootstrap/lib/Panel';
+import axios from 'axios';
+import { createClient } from '@supabase/supabase-js';
 
-//This Component is a child Component of Customers Component
+// Initialize Supabase client
+const supabaseUrl = 'your_supabase_url';
+const supabaseAnonKey = 'your_supabase_anon_key';
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+// This Component is a child Component of Customers Component
 export default class CustomerDetails extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = { customerDetails: null, error: null };
   }
 
-  //Function which is called when the component loads for the first time
+  // Function which is called when the component loads for the first time
   componentDidMount() {
-    this.getCustomerDetails(this.props.val)
+    this.getCustomerDetails(this.props.val);
   }
 
-  //Function which is called whenver the component is updated
+  // Function which is called whenever the component is updated
   componentDidUpdate(prevProps) {
-
-    //get Customer Details only if props has changed
+    // get Customer Details only if props has changed
     if (this.props.val !== prevProps.val) {
-      this.getCustomerDetails(this.props.val)
+      this.getCustomerDetails(this.props.val);
     }
   }
 
-  //Function to Load the customerdetails data from json.
+  // Function to Load the customer details data from JSON.
   getCustomerDetails(id) {
-    axios.get('assets/samplejson/customer' + id + '.json').then(response => {
-      this.setState({customerDetails: response})
-    })
+    axios.get('assets/samplejson/customer' + id + '.json')
+      .then(response => {
+        if (response.data) {
+          this.setState({ customerDetails: response.data });
+          this.saveCustomerDetailsToSupabase(response.data);
+        } else {
+          throw new Error('No data found');
+        }
+      })
+      .catch(error => {
+        this.setState({ error: error.message });
+      });
   };
 
+  // Function to save customer details to Supabase
+  async saveCustomerDetailsToSupabase(customer) {
+    const { data, error } = await supabase
+      .from('customers')
+      .insert([customer]);
+
+    if (error) {
+      this.setState({ error: error.message });
+    } else {
+      console.log('Customer details saved successfully:', data);
+    }
+  }
+
   render() {
-    if (!this.state.customerDetails)
-      return (<p>Loading Data</p>)
-    return (<div className="customerdetails">
+    if (this.state.error) {
+      return <React.Fragment><p>Error: {this.state.error}</p></React.Fragment>;
+    }
+    if (!this.state.customerDetails) {
+      return <React.Fragment><p>Loading Data</p></React.Fragment>;
+    }
+    return <div className="customerdetails">
       <Panel bsStyle="info" className="centeralign">
         <Panel.Heading>
-          <Panel.Title componentClass="h3">{this.state.customerDetails.data.name}</Panel.Title>
+          <Panel.Title componentClass="h3">{this.state.customerDetails.name}</Panel.Title>
         </Panel.Heading>
         <Panel.Body>
-          <p>Name : {this.state.customerDetails.data.name}</p>
-          <p>Email : {this.state.customerDetails.data.email}</p>
-          <p>Phone : {this.state.customerDetails.data.phone}</p>
-          <p>City : {this.state.customerDetails.data.city}</p>
-          <p>State : {this.state.customerDetails.data.state}</p>
-          <p>Country : {this.state.customerDetails.data.country}</p>
-          <p>Organization : {this.state.customerDetails.data.organization}</p>
-          <p>Job Profile : {this.state.customerDetails.data.jobProfile}</p>
-          <p>Additional Info : {this.state.customerDetails.data.additionalInfo}</p>
+          <p>Name : {this.state.customerDetails.name}</p>
+          <p>Email : {this.state.customerDetails.email}</p>
+          <p>Phone : {this.state.customerDetails.phone}</p>
+          <p>City : {this.state.customerDetails.city}</p>
+          <p>State : {this.state.customerDetails.state}</p>
+          <p>Country : {this.state.customerDetails.country}</p>
+          <p>Organization : {this.state.customerDetails.organization}</p>
+          <p>Job Profile : {this.state.customerDetails.jobProfile}</p>
+          <p>Additional Info : {this.state.customerDetails.additionalInfo}</p>
         </Panel.Body>
       </Panel>
-    </div>)
+    </div>;
   }
 }
