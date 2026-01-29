8e8b957343258666ab732564528a35623ea3a1ac
siva-aicoder <p59611423@gmail.com>
2026-01-29 13:06:38 +0000

    Auto: push local modified contents

--- simple-reactjs-app-master/src/CustomerDetails.js
+++ simple-reactjs-app-master/src/CustomerDetails.js
@@ -7,7 +7,10 @@ export default class CustomerDetails extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+      customerDetails: null, // Initialize customerDetails to null
+      showMoreDetails: false // New state to control visibility of additional fields
+    }
   }
 
   //Function which is called when the component loads for the first time
@@ -17,10 +20,13 @@ export default class CustomerDetails extends Component {
 
   //Function which is called whenver the component is updated
   componentDidUpdate(prevProps) {
-
     //get Customer Details only if props has changed
     if (this.props.val !== prevProps.val) {
       this.getCustomerDetails(this.props.val)
+      // Reset showMoreDetails when a new customer is selected
+      if (this.state.showMoreDetails) {
+        this.setState({ showMoreDetails: false });
+      }
     }
   }
 
@@ -29,28 +35,56 @@ export default class CustomerDetails extends Component {
     axios.get('assets/samplejson/customer' + id + '.json').then(response => {
       this.setState({customerDetails: response})
     })
+    .catch(error => {
+      console.error("Error fetching customer details:", error);
+      this.setState({ customerDetails: null }); // Clear details on error
+    });
+  };
+
+  // Toggle function for 'see more' / 'see less' button
+  toggleMoreDetails = () => {
+    this.setState(prevState => ({
+      showMoreDetails: !prevState.showMoreDetails
+    }));
   };
 
   render() {
-    if (!this.state.customerDetails)
-      return (<p>Loading Data</p>)
-    return (<div className="customerdetails">
-      <Panel bsStyle="info" className="centeralign">
-        <Panel.Heading>
-          <Panel.Title componentClass="h3">{this.state.customerDetails.data.name}</Panel.Title>
-        </Panel.Heading>
-        <Panel.Body>
-          <p>Name : {this.state.customerDetails.data.name}</p>
-          <p>Email : {this.state.customerDetails.data.email}</p>
-          <p>Phone : {this.state.customerDetails.data.phone}</p>
-          <p>City : {this.state.customerDetails.data.city}</p>
-          <p>State : {this.state.customerDetails.data.state}</p>
-          <p>Country : {this.state.customerDetails.data.country}</p>
-          <p>Organization : {this.state.customerDetails.data.organization}</p>
-          <p>Job Profile : {this.state.customerDetails.data.jobProfile}</p>
-          <p>Additional Info : {this.state.customerDetails.data.additionalInfo}</p>
-        </Panel.Body>
-      </Panel>
-    </div>)
+    const { customerDetails, showMoreDetails } = this.state;
+
+    if (!customerDetails) {
+      return (<p>Loading Data...</p>);
+    }
+
+    const customer = customerDetails.data; // Access data for cleaner rendering
+
+    return (
+      <div className="customerdetails">
+        <Panel bsStyle="info" className="centeralign">
+          <Panel.Heading>
+            <Panel.Title componentClass="h3">{customer.name}</Panel.Title>
+          </Panel.Heading>
+          <Panel.Body>
+            <p>Name : {customer.name}</p>
+            <p>Email : {customer.email}</p>
+            <p>Phone : {customer.phone}</p>
+            <p>City : {customer.city}</p>
+            <p>State : {customer.state}</p>
+            <p>Country : {customer.country}</p>
+
+            {showMoreDetails && (
+              <React.Fragment>
+                <p>Organization : {customer.organization}</p>
+                <p>Job Profile : {customer.jobProfile}</p>
+                <p>Additional Info : {customer.additionalInfo}</p>
+              </React.Fragment>
+            )}
+
+            <button onClick={this.toggleMoreDetails} className="btn btn-default btn-sm mt-2">
+              {showMoreDetails ? 'see less' : 'see more'}
+            </button>
+          </Panel.Body>
+        </Panel>
+      </div>
+    );
   }
 }
