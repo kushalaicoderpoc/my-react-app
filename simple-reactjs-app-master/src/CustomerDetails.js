0ee225e0cf9f479abffb2237af9f129813e7ad81
siva-aicoder <p59611423@gmail.com>
2026-02-02 13:24:08 +0530

    Refactor CustomerDetails component state management

--- simple-reactjs-app-master/src/CustomerDetails.js
+++ simple-reactjs-app-master/src/CustomerDetails.js
@@ -7,11 +7,7 @@ export default class CustomerDetails extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {
-      customerDetails: null, // Initialize customerDetails
-      showMoreInfo: false, // State to manage visibility of additional info
-    };
-    this.toggleMoreInfo = this.toggleMoreInfo.bind(this);
+    this.state = {}
   }
 
   //Function which is called when the component loads for the first time
@@ -32,48 +28,27 @@ export default class CustomerDetails extends Component {
   getCustomerDetails(id) {
     axios.get('assets/samplejson/customer' + id + '.json').then(response => {
       this.setState({customerDetails: response})
-    });
+    })
   };
 
-  // Toggles the visibility of additional information fields
-  toggleMoreInfo() {
-    this.setState(prevState => ({
-      showMoreInfo: !prevState.showMoreInfo,
-    }));
-  }
-
   render() {
-    if (!this.state.customerDetails || !this.state.customerDetails.data)
-      return (<p>Loading Data</p>);
-
-    const { data } = this.state.customerDetails; // Destructure data for easier access
-    const { showMoreInfo } = this.state; // Get showMoreInfo from state
-
-    return (
-    <div className="customerdetails">
+    if (!this.state.customerDetails)
+      return (<p>Loading Data</p>)
+    return (<div className="customerdetails">
       <Panel bsStyle="info" className="centeralign">
         <Panel.Heading>
-          <Panel.Title componentClass="h3">{data.name}</Panel.Title>
+          <Panel.Title componentClass="h3">{this.state.customerDetails.data.name}</Panel.Title>
         </Panel.Heading>
         <Panel.Body>
-          <p>Name : {data.name}</p>
-          <p>Email : {data.email}</p>
-          <p>Phone : {data.phone}</p>
-          <p>City : {data.city}</p>
-          <p>State : {data.state}</p>
-          <p>Country : {data.country}</p>
-
-          {showMoreInfo && (
-            <React.Fragment>
-              <p>Organization : {data.organization}</p>
-              <p>Job Profile : {data.jobProfile}</p>
-              <p>Additional Info : {data.additionalInfo}</p>
-            </React.Fragment>
-          )}
-
-          <button onClick={this.toggleMoreInfo} className="btn btn-link">
-            {showMoreInfo ? 'See Less' : 'See More'}
-          </button>
+          <p>Name : {this.state.customerDetails.data.name}</p>
+          <p>Email : {this.state.customerDetails.data.email}</p>
+          <p>Phone : {this.state.customerDetails.data.phone}</p>
+          <p>City : {this.state.customerDetails.data.city}</p>
+          <p>State : {this.state.customerDetails.data.state}</p>
+          <p>Country : {this.state.customerDetails.data.country}</p>
+          <p>Organization : {this.state.customerDetails.data.organization}</p>
+          <p>Job Profile : {this.state.customerDetails.data.jobProfile}</p>
+          <p>Additional Info : {this.state.customerDetails.data.additionalInfo}</p>
         </Panel.Body>
       </Panel>
     </div>)
