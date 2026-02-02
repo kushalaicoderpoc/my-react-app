10a28a00755800e39f4d8864aeda39e2f48938e3
siva-aicoder <p59611423@gmail.com>
2026-02-02 07:45:28 +0000

    Auto: push local modified contents

--- simple-reactjs-app-master/src/CustomerDetails.js
+++ simple-reactjs-app-master/src/CustomerDetails.js
@@ -7,7 +7,10 @@ export default class CustomerDetails extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+      customerDetails: null,
+      showMoreInfo: false // New state variable to control visibility
+    }
   }
 
   //Function which is called when the component loads for the first time
@@ -15,12 +18,15 @@ export default class CustomerDetails extends Component {
     this.getCustomerDetails(this.props.val)
   }
 
-  //Function which is called whenver the component is updated
+  //Function which is called whenever the component is updated
   componentDidUpdate(prevProps) {
-
     //get Customer Details only if props has changed
     if (this.props.val !== prevProps.val) {
       this.getCustomerDetails(this.props.val)
+      // Reset showMoreInfo when the customer changes
+      if (this.state.showMoreInfo) {
+        this.setState({ showMoreInfo: false });
+      }
     }
   }
 
@@ -31,24 +37,44 @@ export default class CustomerDetails extends Component {
     })
   };
 
+  // Function to toggle the visibility of additional information fields
+  toggleMoreInfo = () => {
+    this.setState(prevState => ({
+      showMoreInfo: !prevState.showMoreInfo
+    }));
+  };
+
   render() {
     if (!this.state.customerDetails)
       return (<p>Loading Data</p>)
+
+    const { customerDetails, showMoreInfo } = this.state;
+    const customerData = customerDetails.data; // Destructure for cleaner access
+
     return (<div className="customerdetails">
       <Panel bsStyle="info" className="centeralign">
         <Panel.Heading>
-          <Panel.Title componentClass="h3">{this.state.customerDetails.data.name}</Panel.Title>
+          <Panel.Title componentClass="h3">{customerData.name}</Panel.Title>
         </Panel.Heading>
         <Panel.Body>
-          <p>Name : {this.state.customerDetails.data.name}</p>
-          <p>Email : {this.state.customerDetails.data.email}</p>
-          <p>Phone : {this.state.customerDetails.data.phone}</p>
-          <p>City : {this.state.customerDetails.data.city}</p>
-          <p>State : {this.state.customerDetails.data.state}</p>
-          <p>Country : {this.state.customerDetails.data.country}</p>
-          <p>Organization : {this.state.customerDetails.data.organization}</p>
-          <p>Job Profile : {this.state.customerDetails.data.jobProfile}</p>
-          <p>Additional Info : {this.state.customerDetails.data.additionalInfo}</p>
+          <p>Name : {customerData.name}</p>
+          <p>Email : {customerData.email}</p>
+          <p>Phone : {customerData.phone}</p>
+          <p>City : {customerData.city}</p>
+          <p>State : {customerData.state}</p>
+          <p>Country : {customerData.country}</p>
+
+          <button onClick={this.toggleMoreInfo} className="btn btn-link">
+            {showMoreInfo ? 'see less' : 'see more'}
+          </button>
+
+          {showMoreInfo && (
+            <React.Fragment>
+              <p>Organization : {customerData.organization}</p>
+              <p>Job Profile : {customerData.jobProfile}</p>
+              <p>Additional Info : {customerData.additionalInfo}</p>
+            </React.Fragment>
+          )}
         </Panel.Body>
       </Panel>
     </div>)
