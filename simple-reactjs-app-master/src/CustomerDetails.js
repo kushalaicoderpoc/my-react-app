d50816740cf7976cb082ed00822be05a26969639
siva-aicoder <p59611423@gmail.com>
2026-06-16 21:13:04 +0530

    Update CustomerDetails.js

--- simple-reactjs-app-master/src/CustomerDetails.js
+++ simple-reactjs-app-master/src/CustomerDetails.js
@@ -1,76 +1,82 @@
-import React, { useState, useEffect } from "react";
+import React, { Component } from "react";
 import Panel from "react-bootstrap/lib/Panel";
 import axios from "axios";
 
-const CustomerDetails = ({ val }) => {
-  const [customerDetails, setCustomerDetails] = useState(null);
-  const [showMore, setShowMore] = useState(false);
-
-  // Fetch customer data
-  const getCustomerDetails = async (id) => {
-    try {
-      const response = await axios.get(
-        `assets/samplejson/customer${id}.json`
-      );
-      setCustomerDetails(response);
-      setShowMore(false); // reset dropdown when customer changes
-    } catch (error) {
-      console.error("Error fetching customer details:", error);
-    }
-  };
-
-  // Load on mount + when val changes
-  useEffect(() => {
-    if (val) {
-      getCustomerDetails(val);
-    }
-  }, [val]);
-
-  if (!customerDetails) {
-    return <p>Loading Data...</p>;
+export default class CustomerDetails extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      customerDetails: null,
+      showMore: false,
+    };
   }
 
-  const data = customerDetails.data;
+  componentDidMount() {
+    this.getCustomerDetails(this.props.val);
+  }
 
-  return (
-    <div className="customerdetails">
-      <Panel bsStyle="info" className="centeralign">
-        <Panel.Heading>
-          <Panel.Title componentClass="h3">{data.name}</Panel.Title>
-        </Panel.Heading>
+  componentDidUpdate(prevProps) {
+    if (this.props.val !== prevProps.val) {
+      this.getCustomerDetails(this.props.val);
+    }
+  }
 
-        <Panel.Body>
-          <p>Name : {data.name}</p>
-          <p>Email : {data.email}</p>
-          <p>Phone : {data.phone}</p>
-          <p>City : {data.city}</p>
-          <p>State : {data.state}</p>
-          <p>Country : {data.country}</p>
+  getCustomerDetails(id) {
+    axios
+      .get(`assets/samplejson/customer${id}.json`)
+      .then((response) => {
+        this.setState({ customerDetails: response, showMore: false });
+      })
+      .catch((err) => console.error(err));
+  }
 
-          {/* Toggle Button */}
-          <button
-            onClick={() => setShowMore((prev) => !prev)}
-            style={{
-              marginTop: "10px",
-              padding: "6px 12px",
-              cursor: "pointer",
-            }}
-          >
-            {showMore ? "See Less" : "See More"}
-          </button>
+  toggleShowMore = () => {
+    this.setState((prev) => ({ showMore: !prev.showMore }));
+  };
 
-          {/* Expandable Section */}
-          {showMore && (
-            <div style={{ marginTop: "10px" }}>
-              <p>Organization : {data.organization}</p>
-              <p>Job Profile : {data.jobProfile}</p>
-              <p>Additional Info : {data.additionalInfo}</p>
-            </div>
-          )}
-        </Panel.Body>
-      </Panel>
-    </div>
-  );
-};
+  render() {
+    const { customerDetails, showMore } = this.state;
 
-export default CustomerDetails;
+    if (!customerDetails) return <p>Loading Data</p>;
+
+    const data = customerDetails.data;
+
+    return (
+      <div className="customerdetails">
+        <Panel bsStyle="info" className="centeralign">
+          <Panel.Heading>
+            <Panel.Title componentClass="h3">{data.name}</Panel.Title>
+          </Panel.Heading>
+
+          <Panel.Body>
+            <p>Name : {data.name}</p>
+            <p>Email : {data.email}</p>
+            <p>Phone : {data.phone}</p>
+            <p>City : {data.city}</p>
+            <p>State : {data.state}</p>
+            <p>Country : {data.country}</p>
+
+            <button
+              onClick={this.toggleShowMore}
+              style={{
+                marginTop: "10px",
+                padding: "6px 12px",
+                cursor: "pointer",
+              }}
+            >
+              {showMore ? "See Less" : "See More"}
+            </button>
+
+            {showMore && (
+              <div style={{ marginTop: "10px" }}>
+                <p>Organization : {data.organization}</p>
+                <p>Job Profile : {data.jobProfile}</p>
+                <p>Additional Info : {data.additionalInfo}</p>
+              </div>
+            )}
+          </Panel.Body>
+        </Panel>
+      </div>
+    );
+  }
+}
