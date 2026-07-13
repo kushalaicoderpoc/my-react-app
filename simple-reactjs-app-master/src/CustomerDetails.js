201df98e2129d4da61c18d87b44ca7cafb205761
siva-aicoder <p59611423@gmail.com>
2026-07-13 15:56:24 +0530

    Auto: push local modified contents

--- simple-reactjs-app-master/src/CustomerDetails.js
+++ simple-reactjs-app-master/src/CustomerDetails.js
@@ -1,56 +1,72 @@
-import React, {Component} from 'react';
-import Panel from 'react-bootstrap/lib/Panel'
-import axios from 'axios'
+import React, { useState, useEffect } from 'react';
+import Panel from 'react-bootstrap/lib/Panel';
+import axios from 'axios';
 
-//This Component is a child Component of Customers Component
-export default class CustomerDetails extends Component {
+export default function CustomerDetails({ val }) {
+  const [customerDetails, setCustomerDetails] = useState(null);
+  const [showMore, setShowMore] = useState(false);
 
-  constructor(props) {
-    super(props);
-    this.state = {}
-  }
-
-  //Function which is called when the component loads for the first time
-  componentDidMount() {
-    this.getCustomerDetails(this.props.val)
-  }
-
-  //Function which is called whenver the component is updated
-  componentDidUpdate(prevProps) {
-
-    //get Customer Details only if props has changed
-    if (this.props.val !== prevProps.val) {
-      this.getCustomerDetails(this.props.val)
-    }
-  }
-
-  //Function to Load the customerdetails data from json.
-  getCustomerDetails(id) {
-    axios.get('assets/samplejson/customer' + id + '.json').then(response => {
-      this.setState({customerDetails: response})
-    })
+  const getCustomerDetails = (id) => {
+    axios.get(`assets/samplejson/customer${id}.json`)
+      .then(response => {
+        setCustomerDetails(response);
+      })
+      .catch(error => {
+        console.error("Error fetching customer details:", error);
+        setCustomerDetails(null); // Reset or handle error state
+      });
   };
 
-  render() {
-    if (!this.state.customerDetails)
-      return (<p>Loading Data</p>)
-    return (<div className="customerdetails">
+  // useEffect combines componentDidMount and componentDidUpdate logic
+  useEffect(() => {
+    if (val) {
+      getCustomerDetails(val);
+    }
+  }, [val]); // Re-run effect only if 'val' prop changes
+
+  if (!customerDetails) {
+    return <p>Loading Data</p>;
+  }
+
+  return (
+    <div className="customerdetails">
       <Panel bsStyle="info" className="centeralign">
         <Panel.Heading>
-          <Panel.Title componentClass="h3">{this.state.customerDetails.data.name}</Panel.Title>
+          <Panel.Title componentClass="h3">{customerDetails.data.name}</Panel.Title>
         </Panel.Heading>
         <Panel.Body>
-          <p>Name : {this.state.customerDetails.data.name}</p>
-          <p>Email : {this.state.customerDetails.data.email}</p>
-          <p>Phone : {this.state.customerDetails.data.phone}</p>
-          <p>City : {this.state.customerDetails.data.city}</p>
-          <p>State : {this.state.customerDetails.data.state}</p>
-          <p>Country : {this.state.customerDetails.data.country}</p>
-          <p>Organization : {this.state.customerDetails.data.organization}</p>
-          <p>Job Profile : {this.state.customerDetails.data.jobProfile}</p>
-          <p>Additional Info : {this.state.customerDetails.data.additionalInfo}</p>
+          <p>Name : {customerDetails.data.name}</p>
+          <p>Email : {customerDetails.data.email}</p>
+          <p>Phone : {customerDetails.data.phone}</p>
+          <p>City : {customerDetails.data.city}</p>
+          <p>State : {customerDetails.data.state}</p>
+          <p>Country : {customerDetails.data.country}</p>
+
+          {showMore && (
+            <React.Fragment>
+              <p>Organization : {customerDetails.data.organization}</p>
+              <p>Job Profile : {customerDetails.data.jobProfile}</p>
+              <p>Additional Info : {customerDetails.data.additionalInfo}</p>
+            </React.Fragment>
+          )}
+
+          <button
+            onClick={() => setShowMore(!showMore)}
+            style={{
+              marginTop: '15px',
+              padding: '8px 15px',
+              backgroundColor: '#007bff',
+              color: 'white',
+              border: 'none',
+              borderRadius: '4px',
+              cursor: 'pointer',
+              fontSize: '14px'
+            }}
+          >
+            {showMore ? 'See Less' : 'See More'}
+          </button>
         </Panel.Body>
       </Panel>
-    </div>)
-  }
+    </div>
+  );
 }
